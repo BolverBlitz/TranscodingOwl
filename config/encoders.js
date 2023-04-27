4e1775f7e318d62aa5805f206afe10659c2913f2
BolverBlitz <bolverblitz@ebg.pw>
2023-04-27 20:22:30 +0000

    Fixed wrong flags for gpu encoders

--- config/encoders.js
+++ config/encoders.js
@@ -12,8 +12,8 @@ module.exports = {
         "name": "hevc_nvenc",
         "type": "GPU",
         "quality": "vbr",
-        "quality_command": "-rc 1",
-        "quality_constract": ["-qp"],
+        "quality_command": "-rc:v vbr",
+        "quality_constract": ["-cq"],
         "quality_preset_command": "-preset",
         "quality_preset_constract": ["fast", "medium", "slow"]
     },
@@ -21,8 +21,8 @@ module.exports = {
         "name": "hevc_amf",
         "type": "GPU",
         "quality": "cqp",
-        "quality_command": "-rc 0",
-        "quality_constract": ["-qp_p", "-qp_i"],
+        "quality_command": "-rc cqp",
+        "quality_constract": ["-qp"],
         "quality_preset_command": "-quality",
         "quality_preset_constract": ["speed", "balanced", "quality"]
     },
